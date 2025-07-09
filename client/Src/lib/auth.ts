28bfd64f04efd41b97fd47cf2e5844cc17807137
body-16 <abdallahelsayde1@gmail.com>
2025-07-10 02:51:25 +0300

    Add files via upload

--- client/Src/lib/auth.ts
+++ client/Src/lib/auth.ts
@@ -1,1 +1,12 @@
-//test 
+export const getAuthToken = () => {
+  return localStorage.getItem("token");
+};
+
+export const setAuthHeader = () => {
+  const token = getAuthToken();
+  return token ? { Authorization: `Bearer ${token}` } : {};
+};
+
+export const isAuthenticated = () => {
+  return !!getAuthToken();
+};
