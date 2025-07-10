5f8c9a6b369de646e968669bc33bd17f1e7cb5cd
body-16 <abdallahelsayde1@gmail.com>
2025-07-10 07:17:01 +0300

    Update main.tsx

--- client/Src/main.tsx
+++ client/Src/main.tsx
@@ -1,9 +1,9 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Failed to find the root element");
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
 
 const root = createRoot(rootElement);
 root.render(<App />);
